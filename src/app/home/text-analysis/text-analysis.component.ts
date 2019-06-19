f962e275754d010976e543b24075793b8f82af30
Danil27 <danil27dzr@gmail.com>
2019-06-19 14:47:29 +0300

    1906

--- src/app/home/text-analysis/text-analysis.component.ts
+++ src/app/home/text-analysis/text-analysis.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClientHome } from '../httpHome.service';
 
 @Component({
   selector: 'app-text-analysis',
   templateUrl: './text-analysis.component.html',
-  styleUrls: ['./text-analysis.component.css']
+  styleUrls: ['./text-analysis.component.css'],
+  providers: [HttpClientHome]
 })
 export class TextAnalysisComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClientHome) { }
 
   ngOnInit() {
   }
 
   getText(form: NgForm) {
-    console.log(form.value);
+    //console.log(form.value.text);
+    this.httpClient.getData("settext/?text=" + form.value.text).subscribe();
   }
 
 }
